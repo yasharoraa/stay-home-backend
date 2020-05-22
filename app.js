90a13ea37381a4c9c257fbc4c5e0e849e0988fb9
Yash Arora <yash.arora1704@gmail.com>
2020-05-22 12:51:55 +0530

    unified topology

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-const connect = mongoose.connect(mongoUrl, {useNewUrlParser: true});
+const connect = mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 connect.then((db) => {
     console.log('Connected correctly to server');
