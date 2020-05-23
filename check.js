d054c6c76bfa715831bf33d9fd720a2d177e4466
Yash Arora <yash.arora1704@gmail.com>
2020-05-23 18:22:05 +0530

    headers check

--- check.js
+++ check.js
@@ -1,5 +1,7 @@
 module.exports = function(req,res,next){
     if(process.env.NODE_ENV === "production"){
+        console.log(req.headers.lot_pot);
+        console.log(req.headers.kit_mit);
         if(req.headers.lot_pot === process.env.CLIENT && req.headers.kit_mit === process.env.CLIENT_SECRET){            
             next();
         }else{
