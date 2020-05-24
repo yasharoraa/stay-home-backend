cad7e4dffae56e06978171fa40d8a1d8a7316656
Yash Arora <yash.arora1704@gmail.com>
2020-05-24 15:11:31 +0530

    api headers remove

--- check.js
+++ check.js
@@ -1,7 +1,5 @@
 module.exports = function(req,res,next){
     if(process.env.NODE_ENV === "production"){
-        console.log(req.headers.lot_pot);
-        console.log(req.headers.kit_mit);
         if(req.headers.lot_pot === process.env.CLIENT && req.headers.kit_mit === process.env.CLIENT_SECRET){            
             next();
         }else{
